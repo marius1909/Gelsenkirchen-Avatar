96df5dc6ce30f1dd0ae44f9c78f04fe7b8cc9993
MissFrionta <69893925+MissFrionta@users.noreply.github.com>
2020-11-02 17:03:25 +0100

    Einen unused import auskommentiert und fluttertoast: ^7.1.1 in pubspec.yaml auskommentiert

--- gelsenkirchen_avatar/ios/Runner/GeneratedPluginRegistrant.m
+++ gelsenkirchen_avatar/ios/Runner/GeneratedPluginRegistrant.m
@@ -4,9 +4,37 @@
 
 #import "GeneratedPluginRegistrant.h"
 
+#if __has_include(<fluttertoast/FluttertoastPlugin.h>)
+#import <fluttertoast/FluttertoastPlugin.h>
+#else
+@import fluttertoast;
+#endif
+
+#if __has_include(<location/LocationPlugin.h>)
+#import <location/LocationPlugin.h>
+#else
+@import location;
+#endif
+
+#if __has_include(<path_provider/FLTPathProviderPlugin.h>)
+#import <path_provider/FLTPathProviderPlugin.h>
+#else
+@import path_provider;
+#endif
+
+#if __has_include(<sqflite/SqflitePlugin.h>)
+#import <sqflite/SqflitePlugin.h>
+#else
+@import sqflite;
+#endif
+
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FluttertoastPlugin registerWithRegistrar:[registry registrarForPlugin:@"FluttertoastPlugin"]];
+  [LocationPlugin registerWithRegistrar:[registry registrarForPlugin:@"LocationPlugin"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+  [SqflitePlugin registerWithRegistrar:[registry registrarForPlugin:@"SqflitePlugin"]];
 }
 
 @end
--- gelsenkirchen_avatar/lib/registrierung.dart
+++ gelsenkirchen_avatar/lib/registrierung.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+//import 'package:flutter/services.dart';
 import 'package:gelsenkirchen_avatar/anmeldung.dart';
 import 'package:http/http.dart' as http;
 import 'package:fluttertoast/fluttertoast.dart';
