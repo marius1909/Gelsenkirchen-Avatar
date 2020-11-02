d423454b89efe1b8a0cc8f7ae337fb29baa40520
MissFrionta <69893925+MissFrionta@users.noreply.github.com>
2020-11-02 18:16:59 +0100

    Impressum eingefügt

--- gelsenkirchen_avatar/lib/impressum_screen.dart
+++ gelsenkirchen_avatar/lib/impressum_screen.dart
@@ -8,6 +8,11 @@ class ImpressumScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Impressum'),
       ),
+      body: Center(
+          child: Text(
+        "Dies ist eine reine Prototypenanwendung, sie ist nicht für die öffentliche Nutzung bestimmt. \n \nDiese Anwendung ist entstanden als Projekt der Westfählischen Hochschule \n\nDieser Prototyp wurde für die Stadt Gelsenkirchen und ihre Teilnahme an dem Wettbewerbt Zukunftsstadt 2030 erstellt.",
+        textAlign: TextAlign.center,
+      )),
     );
   }
 }
