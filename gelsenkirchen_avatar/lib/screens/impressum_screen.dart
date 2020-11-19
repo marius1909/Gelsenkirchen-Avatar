84400929930ae586d8d0dc9cbe3bc9ff4ac69117
Tobi <TobiasHoffmann522@googlemail.com>
2020-11-19 12:03:50 +0100

    commit test

--- gelsenkirchen_avatar/lib/screens/impressum_screen.dart
+++ gelsenkirchen_avatar/lib/screens/impressum_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:gelsenkirchen_avatar/widgets/nav-drawer.dart';
 
+//Sinnloser Kommentar um einen neuen Branche zu starten damit ich den schon mal machen kann und nicht ausversehen das direket in den Master schicke 
+//TEST
+
 class ImpressumScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
