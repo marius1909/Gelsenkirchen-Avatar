928b61813b866be1e9853a3a919a9d60ce67a003
Marius <marius.weise@web.de>
2020-12-07 15:16:19 +0100

    More usability, add friend function

---
+++ gelsenkirchen_avatar/lib/screens/testfreund.dart
@@ -0,0 +1,14 @@
+// ignore: must_be_immutable
+class  Testfreund  {
+
+    String name;
+   String level;
+   String freundschaftslevel;
+
+Testfreund(String name, String level, String freundschaftslevel) {
+  this.name = name;
+  this.level = level;
+  this.freundschaftslevel = freundschaftslevel;
+}
+
+}
