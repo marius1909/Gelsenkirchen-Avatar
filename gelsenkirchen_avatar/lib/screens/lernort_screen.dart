911f1f5b338ef0c8bf325db76ffd8966fea5a3a5
phthao123 <69897117+phthao123@users.noreply.github.com>
2020-12-09 17:29:11 +0100

    Update lernort_screen.dart

--- gelsenkirchen_avatar/lib/screens/lernort_screen.dart
+++ gelsenkirchen_avatar/lib/screens/lernort_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gelsenkirchen_avatar/data/lernort.dart';
 import 'package:gelsenkirchen_avatar/quiz/start_quiz.dart';
 import 'package:gelsenkirchen_avatar/screens/lernen_screen.dart';
+import 'package:gelsenkirchen_avatar/quiz/quiz.dart';
 
 class LernortScreen extends StatelessWidget {
   final Lernort l;
