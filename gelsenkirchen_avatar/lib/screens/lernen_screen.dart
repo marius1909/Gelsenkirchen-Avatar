7c50119cf2347c8b5d3a210c043512750a4e3ebc
Tobi <TobiasHoffmann522@googlemail.com>
2020-12-17 12:48:32 +0100

    Lerninhalte hinzugefügt

--- gelsenkirchen_avatar/lib/screens/lernen_screen.dart
+++ gelsenkirchen_avatar/lib/screens/lernen_screen.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 //import 'package:gelsenkirchen_avatar/widgets/nav-drawer.dart';
 import 'package:gelsenkirchen_avatar/data/lernort.dart';
 import 'package:photo_view/photo_view.dart';
+import 'package:video_player/video_player.dart';
+import 'package:gelsenkirchen_avatar/widgets/chewie_list_item.dart';
+
 
 //ToDo Weitere Medien einfügen
 
@@ -20,21 +23,16 @@ class Lernen extends StatelessWidget {
             /*TITELBILD*/
             Container(child: getWidgetTitelbild(l)),
 
-            /*Beschreibung*/
+            /*TABS*/
             Container(
-                child: Text(
-                  l.beschreibung.replaceAll('<br>', '\n'),
-                  textAlign: TextAlign.justify,
-                  style: TextStyle(fontSize: 17),
-                ),
-                margin: EdgeInsets.only(
-                    left: 30.0, top: 40.0, right: 30.0, bottom: 10.0)),
+              child: getWidgetTabs(l, context),
+            ),
 
-            /*Weitere Bilder*/
+            /*Videos
             Container(
-              child: getWidgetWeitereBilder(l, context),
+              child: getWidgetVideos(l, context),
               height: 400,
-            )
+            ),*/
           ]),
         ));
   }
@@ -49,6 +47,147 @@ Widget getWidgetTitelbild(Lernort l) {
   }
 }
 
+Widget getWidgetTabs(Lernort l, BuildContext context) {
+  int anzahl = 0;
+  bool text = false;
+  bool videos = false;
+  bool sounds = false;
+  bool bilder = false;
+
+  if (!l.beschreibung.isEmpty) {
+    anzahl++;
+    text = true;
+  }
+  if (!l.videos.isEmpty) {
+    anzahl++;
+    videos = true;
+  }
+  if (!l.sounds.isEmpty) {
+    anzahl++;
+    sounds = true;
+  }
+  if (!l.weitereBilder.isEmpty) {
+    anzahl++;
+    bilder = true;
+  }
+  if (anzahl > 0) {
+    return DefaultTabController(
+        length: 4, // length of tabs
+        initialIndex: 0,
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                child: TabBar(
+                  labelColor: Colors.green,
+                  unselectedLabelColor: Colors.black,
+                  tabs: [
+                    get_TabsTitel(
+                      1,
+                      true,
+                    ),
+                    get_TabsTitel(
+                      2,
+                      true,
+                    ),
+                    get_TabsTitel(
+                      3,
+                      true,
+                    ),
+                    get_TabsTitel(
+                      4,
+                      true,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                  height: 400, //height of TabBarView
+                  decoration: BoxDecoration(
+                      border: Border(
+                          top: BorderSide(color: Colors.grey, width: 0.5))),
+                  child: TabBarView(children: <Widget>[
+                    Container(
+                      child: Center(
+                        child: Text(
+                          l.beschreibung.replaceAll('<br>', '\n'),
+                          textAlign: TextAlign.justify,
+                          style: TextStyle(fontSize: 17),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: Center(
+                        child: getWidgetVideos(l, context),
+                      ),
+                    ),
+                    Container(
+                      child: Center(
+                        child: getWidgetSound(l, context),
+                      ),
+                    ),
+                    Container(
+                        child: Center(
+                          child: getWidgetWeitereBilder(l, context),
+                        ),
+                        height: 400),
+                  ]))
+            ]));
+  }
+}
+
+Widget get_TabsTitel(int i, bool sollRein) {
+  //1 = Text  //2= Video   //3 = Sounds    //4= Bilder
+  if (sollRein) {
+    if (i == 1) {
+      return Tab(text: 'Text', icon: Icon(Icons.list));
+    } else if (i == 2) {
+      return Tab(text: 'Videos', icon: Icon(Icons.video_label));
+    } else if (i == 3) {
+      return Tab(text: 'Sounds', icon: Icon(Icons.voice_chat));
+    } else if (i == 4) {
+      return Tab(text: 'Bilder', icon: Icon(Icons.image));
+    } else if (i == 0) {
+      return Tab(text: 'Fehler', icon: Icon(Icons.error));
+    }
+  } else {
+    return null;
+  }
+}
+
+Widget getWidgetSound(Lernort l, BuildContext context) {
+  if (l.sounds.isEmpty) {
+    return new Text(
+      'Leider sind keine Sounds Vorhanden',
+      textAlign: TextAlign.justify,
+      style: TextStyle(fontSize: 17),
+    );
+  } else {
+    return new Text(
+      'Leider wird diese Funktion derzeit noch nicht Unterstützt',
+      textAlign: TextAlign.justify,
+      style: TextStyle(fontSize: 17),
+    );
+  }
+}
+
+Widget getWidgetVideos(Lernort l, BuildContext context) {
+  if (l.weitereBilder.isEmpty) {
+    return new Text(
+      'Leider sind keine Weiteren Bilder Vorhanden',
+      textAlign: TextAlign.justify,
+      style: TextStyle(fontSize: 17),
+    );
+  } else {
+    return ChewieListItem(
+        videoPlayerController: VideoPlayerController.network(
+          //'https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4',
+          l.videos,
+        ),
+        looping: false);
+  }
+}
+
 Widget getWidgetWeitereBilder(Lernort l, BuildContext context) {
   if (l.weitereBilder.isEmpty) {
     return new Text(
@@ -58,7 +197,9 @@ Widget getWidgetWeitereBilder(Lernort l, BuildContext context) {
     );
   } else {
     var arr = l.weitereBilder.split('; '); //Trennzeichen für Die links
+
     var i = 0;
+
     var ii = 0;
     return new GridView.builder(
       itemCount: arr.length,
@@ -68,7 +209,7 @@ Widget getWidgetWeitereBilder(Lernort l, BuildContext context) {
         return new Card(
           child: new InkResponse(
             child: Image.network(
-              arr[i++],
+              arr[index],
               fit: BoxFit.scaleDown,
             ),
             onTap: () {
---
+++ gelsenkirchen_avatar/lib/unused/videoPlayerWidget.dart
@@ -0,0 +1,88 @@
+import 'dart:async';
+
+import 'package:gelsenkirchen_avatar/widgets/fade_animation.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoWidget extends StatefulWidget {
+  @override
+  VideoWidgetState createState() => VideoWidgetState();
+}
+
+class VideoWidgetState extends State<VideoWidget> {
+  VideoPlayerController _controller;
+  bool _isPlaying = false;
+
+  Widget videoStatusAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    videoStatusAnimation = Container();
+
+    _controller = VideoPlayerController.network(
+      'https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4',
+    )
+      ..addListener(() {
+        final bool isPlaying = _controller.value.isPlaying;
+        if (isPlaying != _isPlaying) {
+          setState(() {
+            _isPlaying = isPlaying;
+          });
+        }
+      })
+      ..initialize().then((_) {
+        Timer(Duration(milliseconds: 1500), () {
+          if (!mounted) return;
+
+          setState(() {});
+          _controller.play();
+        });
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => AspectRatio(
+      aspectRatio: 16 / 9,
+      child: _controller.value.initialized ? videoPlayer() : null);
+
+  Widget videoPlayer() => Stack(
+        children: <Widget>[
+          video(),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: VideoProgressIndicator(
+              _controller,
+              allowScrubbing: true,
+              padding: EdgeInsets.all(16.0),
+            ),
+          ),
+          Center(child: videoStatusAnimation),
+        ],
+      );
+
+  Widget video() => GestureDetector(
+        child: VideoPlayer(_controller),
+        onTap: () {
+          if (!_controller.value.initialized) {
+            return;
+          }
+          if (_controller.value.isPlaying) {
+            videoStatusAnimation =
+                FadeAnimation(child: const Icon(Icons.pause, size: 100.0));
+            _controller.pause();
+          } else {
+            videoStatusAnimation =
+                FadeAnimation(child: const Icon(Icons.play_arrow, size: 100.0));
+            _controller.play();
+          }
+        },
+      );
+}
