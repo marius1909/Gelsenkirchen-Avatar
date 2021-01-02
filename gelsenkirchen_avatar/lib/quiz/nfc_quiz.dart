e66adfabf0198cdc42c54a53af22ebb34993aa9a
Marius <marius.weise@web.de>
2021-01-02 20:58:12 +0100

    Ergebnisprüfung, Frage und Tipps anzeigen, Nächste Frage Button

--- gelsenkirchen_avatar/lib/quiz/nfc_quiz.dart
+++ gelsenkirchen_avatar/lib/quiz/nfc_quiz.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:gelsenkirchen_avatar/widgets/nav-drawer.dart';
 
@@ -7,25 +9,62 @@ class NFCQuiz extends StatefulWidget {
 }
 
 class _NFCQuizState extends State<NFCQuiz> {
-  List<Flexible> buchstabenFelder;
 
-  List<String> wortList = new List();
+
+  String ergebnisText = "";
+  String loesungswort = "";
+  String frage = "";
+  List<String> tipps = new List();
+  int anzahlAnGezeigtenTipps;
+
+ 
+
+  //wird false wenn neue Frage gestellt wird
+  bool initComplete = false;
+  bool frageAngezeigt = false;
+
+  List<Flexible> buchstabenFelder;
+  List<TextEditingController> alleTextController;
 
   Widget build(BuildContext context) {
-    String loesungswort = "Rotbuche";
-    wortList = loesungswort.split("");
-    print(wortList);
+    //init---------------
+    if (!initComplete) {
+      alleTextController = new List();
+      buchstabenFelder = buildTextFields();
 
-    buchstabenFelder = buildTextFields();
+      initComplete = true;
+    } //init end-----------
 
     return Scaffold(
       drawer: NavDrawer(),
       appBar: AppBar(
         title: Text('NFC QUIZ'),
       ),
-      body: Row(
+      body: Column(
         children: [
-          for (int i = 0; i < buchstabenFelder.length; i++) buchstabenFelder[i]
+          Timer
+          FlatButton(
+              color: Colors.blue,
+              onPressed: () => neueFrage(),
+              child: Text("Nächste Frage")),
+          Text(frage, style: TextStyle(fontSize: 20)),
+          Column(
+            children: [
+              if (frageAngezeigt)
+                for (int i = 0; i < anzahlAnGezeigtenTipps; i++) Text(tipps[i])
+            ],
+          ),
+          Row(
+            children: [
+              for (int i = 0; i < buchstabenFelder.length; i++)
+                buchstabenFelder[i]
+            ],
+          ),
+          FlatButton(
+              color: Colors.blue,
+              onPressed: () => pruefeErgebnis(),
+              child: Text("Prüfe")),
+          Text(ergebnisText)
         ],
       ),
     );
@@ -34,12 +73,68 @@ class _NFCQuizState extends State<NFCQuiz> {
   List<Flexible> buildTextFields() {
     List<Flexible> temp = new List();
 
-    for (int i = 0; i < wortList.length; i++) {
+    for (int i = 0; i < loesungswort.length; i++) {
+      TextEditingController controller = new TextEditingController();
+
+      alleTextController.add(controller);
+
       temp.add(Flexible(
           child: Padding(
-              padding: const EdgeInsets.all(8.0), child: new TextFormField())));
+              padding: const EdgeInsets.all(8.0),
+              child: new TextFormField(
+                controller: controller,
+                //folgendes funktioniert zurzeit nicht und ist ein known bug in dart
+                //https://github.com/flutter/flutter/issues/67236
+                //Es sollte verhindern das man mehr als einen Buchstaben eingibt
+                maxLength: 1,
+                maxLengthEnforced: true,
+              ))));
     }
 
     return temp;
+  }
+
+  bool pruefeErgebnis() {
+    bool wortGleich = true;
+
+    if (loesungswort.isNotEmpty) {
+      for (int i = 0; i < loesungswort.length; i++) {
+        if (!(alleTextController[i].text == loesungswort[i])) {
+          wortGleich = false;
+        }
+      }
+    }
+
+    if (wortGleich) {
+      setState(() {
+        ergebnisText = "Richtig";
+      });
+    } else {
+      setState(() {
+        ergebnisText = "Falsch";
+      });
+    }
+
+    return wortGleich;
+  }
+
+//dummy funktionen für Datenbank implementierung, neueFrage(String Frage, String Loesung, List<String> tipps)?
+  void neueFrage() {
+    setState(() {
+      tipps.clear();
+
+      frage = "Wie heißt Baum X?";
+      loesungswort = "Rotbuche";
+      tipps.add("Es ist ein Baum von Gattung X");
+      tipps.add("Der Baum ist Rot");
+      tipps.add("Der Baum ist eine Buche");
+    });
+
+    anzahlAnGezeigtenTipps = tipps.length;
+
+    print(anzahlAnGezeigtenTipps);
+
+    frageAngezeigt = true;
+    initComplete = false;
   }
 }
