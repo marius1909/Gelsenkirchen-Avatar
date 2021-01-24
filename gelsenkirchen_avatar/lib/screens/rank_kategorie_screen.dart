79f8d4eb9f54c76c5d587a26200ee38910a9edc3
MissFrionta <lisa.nar@outlook.de>
2021-01-24 15:54:34 +0100

    ErsteDesignanpassungen auf KategorieRankingScreen

--- gelsenkirchen_avatar/lib/screens/rank_kategorie_screen.dart
+++ gelsenkirchen_avatar/lib/screens/rank_kategorie_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter_icons/flutter_icons.dart';
 
 class RankKategorieScreen extends StatefulWidget {
   int id_lern_category;
@@ -48,6 +49,7 @@ class _RankKategorieScreenState extends State<RankKategorieScreen> {
   @override
   Widget build(BuildContext context) {
     if (data == null) {
+      /* Hier wäre ne Info für den Benutzer super ;) */
       return Scaffold(
           body: new Container(
               margin: EdgeInsets.all(10.0),
@@ -62,30 +64,43 @@ class _RankKategorieScreenState extends State<RankKategorieScreen> {
           ),
           body: Column(
             children: [
-              Padding(
-                padding: EdgeInsets.only(top: 10),
-              ),
-              RichText(
-                text: TextSpan(
-                    style: TextStyle(color: Colors.black, fontSize: 17),
-                    children: <TextSpan>[
-                      TextSpan(text: "Your Rank : "),
-                      TextSpan(
-                          text: data['current_rank'].toString(),
-                          style: TextStyle(color: Colors.red)),
-                    ]),
-              ),
-              RichText(
-                text: TextSpan(
-                    style: TextStyle(color: Colors.black, fontSize: 17),
-                    children: <TextSpan>[
-                      TextSpan(text: "Your Score : "),
-                      TextSpan(
-                          text: data['current_point'].toString(),
-                          style: TextStyle(color: Colors.red)),
-                    ]),
-              ),
-              SizedBox(
+              Container(
+                  padding: EdgeInsets.fromLTRB(15, 40, 15, 40),
+                  child: Column(children: [
+                    Row(
+                      children: [
+                        /* TODO: Vielleicht ein anderes Icon (Lisa) */
+                        Icon(FlutterIcons.arrow_up_faw5s,
+                            size: 20, color: Color(0xff0b3e99)),
+                        SizedBox(width: 10),
+                        Text("Dein Level: " + data['current_rank'].toString(),
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context).textTheme.headline4),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Icon(
+                          FlutterIcons.coin_mco,
+                          color: Color(0xffff9f1c),
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                            "Deine Erfahrungspunkte: " +
+                                data['current_point'].toString(),
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline4
+                                .apply(color: Color(0xffff9f1c))),
+                      ],
+                    )
+                  ])),
+
+              /* Tabelle mit dem Ranking */
+              Container(
+                  child: SizedBox(
                 width: double.infinity,
                 child: DataTable(
                     columns: const <DataColumn>[
@@ -134,7 +149,7 @@ class _RankKategorieScreenState extends State<RankKategorieScreen> {
                         ],
                       ),
                     )),
-              ),
+              ))
             ],
           ),
         );
@@ -143,7 +158,7 @@ class _RankKategorieScreenState extends State<RankKategorieScreen> {
             appBar: AppBar(
               title: Text(widget.name_lern_category),
             ),
-            body: new Container(
+            body: Container(
                 margin: EdgeInsets.all(10.0),
                 alignment: Alignment.topCenter,
                 child: Center(
