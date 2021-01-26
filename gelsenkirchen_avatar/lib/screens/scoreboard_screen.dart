3bbb8702b522fedd9a1339882bb33ccbe2a9465d
phthao123 <69897117+phthao123@users.noreply.github.com>
2021-01-26 03:40:22 +0100

    "TODO: Kategoriesymbol einfügen" schon erledigt

--- gelsenkirchen_avatar/lib/screens/scoreboard_screen.dart
+++ gelsenkirchen_avatar/lib/screens/scoreboard_screen.dart
@@ -103,15 +103,96 @@ class _ScoreBoardState extends State<ScoreBoard> {
 
   /*Diese Methode erstellt die ListViewItems*/
   Widget erstelleListViewitem(BuildContext context, int index) {
+    Icon kategorienSymbol;
+    Color symbolcolor = Color(0xff0b3e99);
+    double symbolsize = 25;
+    switch (int.parse(data[index]['id'])) {
+      case 0:
+        {
+          kategorienSymbol = Icon(FlutterIcons.cube_faw5s,
+              size: symbolsize, color: symbolcolor);
+        }
+        break;
+
+      case 1:
+        {
+          kategorienSymbol = Icon(FlutterIcons.compass_faw5s,
+              size: symbolsize, color: symbolcolor);
+        }
+        break;
+
+      case 2:
+        {
+          kategorienSymbol = Icon(FlutterIcons.seedling_faw5s,
+              size: symbolsize, color: symbolcolor);
+        }
+        break;
+
+      case 3:
+        {
+          kategorienSymbol = Icon(FlutterIcons.futbol_faw5s,
+              size: symbolsize, color: symbolcolor);
+        }
+        break;
+
+      case 4:
+        {
+          kategorienSymbol = Icon(FlutterIcons.palette_faw5s,
+              size: symbolsize, color: symbolcolor);
+        }
+        break;
+
+      case 5:
+        {
+          kategorienSymbol = Icon(FlutterIcons.temperature_low_faw5s,
+              size: symbolsize, color: symbolcolor);
+        }
+        break;
+
+      case 6:
+        {
+          kategorienSymbol = Icon(FlutterIcons.book_faw5s,
+              size: symbolsize, color: symbolcolor);
+        }
+        break;
+
+      case 7:
+        {
+          kategorienSymbol = Icon(FlutterIcons.hand_holding_heart_faw5s,
+              size: symbolsize, color: symbolcolor);
+        }
+        break;
+
+      case 8:
+        {
+          kategorienSymbol = Icon(FlutterIcons.music_faw5s,
+              size: symbolsize, color: symbolcolor);
+        }
+        break;
+
+      case 9:
+        {
+          kategorienSymbol = Icon(FlutterIcons.laptop_code_faw5s,
+              size: symbolsize, color: symbolcolor);
+        }
+        break;
+      default:
+        {
+          kategorienSymbol =
+              Icon(Icons.category, size: symbolsize, color: symbolcolor);
+        }
+    }
+
     return new Card(
         child: new Column(
       children: <Widget>[
         new ListTile(
           title: Row(
-            children: [
-              /* TODO: Kategoriesymbol einfügen. Vgl. kategorie_top_tab.dart, dort wurde das auch schon mal gemacht. (Lisa) */
-              //kategorienSymbol,
-
+            children: [              
+              kategorienSymbol,
+              SizedBox(
+                width: 25,
+              ),
               Text(
                 data[index]['name'].toString(),
                 style: Theme.of(context).textTheme.bodyText1,
