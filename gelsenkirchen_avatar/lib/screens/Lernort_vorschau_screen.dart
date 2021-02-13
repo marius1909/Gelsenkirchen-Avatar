d4621456a3992c83c8e6d83b13ea0f2a51b3101c
MissFrionta <lisa.nar@outlook.de>
2021-02-13 13:17:34 +0100

    In Lernort-Vorschau aufgeräumt ;)

--- gelsenkirchen_avatar/lib/screens/Lernort_vorschau_screen.dart
+++ gelsenkirchen_avatar/lib/screens/Lernort_vorschau_screen.dart
@@ -147,181 +147,85 @@ class _LernortVorschauState extends State<LernortVorschau> {
               child: Center(child: CircularProgressIndicator())));
     } else {
       return Scaffold(
-        appBar: AppBar(
-          title: Text(lernort.name),
-        ),
-        //   body: SafeArea(
-        // child:
-        body: SingleChildScrollView(
-            child: Column(children: [
-          /* TITELBILD */
-          Container(child: titelbild),
-          Container(
-            padding: new EdgeInsets.all(15.0),
-            child: Column(
-              children: [
-                /*
-                /* KATEGORIE*/
-                /* TODO: - Hier muss noch der Kategoriename anstatt die KategorieId eingefügt werden */
-                /* TODO: Kategorieicon anzeigen (Lisa) */
-                /* TODO: Kategoriename aus DB anzeigen (Lisa) */
-                Row(children: [
-                  kategorienSymbol,
-                  SizedBox(width: 10),
-                  /* TODO: "lernKategorieList[lernort.kategorieID].name" verursacht einen Fehler, von dem ich nicht weiß, wie ich ihn beheben soll. Deshalb auch auskommentiert. (Lisa) */
-                  Text("Kategorie",
-                      //lernKategorieList[lernort.kategorieID].name,
-                      style: Theme.of(context).textTheme.headline4),
-                ]),
-                SizedBox(height: 20),
-
-                /* ADRESSE */
-                Row(children: [
-                  Icon(FlutterIcons.location_arrow_faw5s,
-                      size: 20, color: Color(0xff0b3e99)),
-                  SizedBox(width: 10),
-                  Flexible(
-                    child: Text(
-                        lernort.adresse == ""
-                            ? "Keine Adresse vorhanden"
-                            : lernort.adresse,
+          appBar: AppBar(
+            title: Text(lernort.name),
+          ),
+          body: SingleChildScrollView(
+              child: Column(children: [
+            /* TITELBILD */
+            Container(child: titelbild),
+            Container(
+                padding: new EdgeInsets.all(15.0),
+                child: Column(children: [
+                  /* KATEGORIE*/
+                  Row(children: [
+                    kategorienSymbol,
+                    SizedBox(width: 10),
+                    /* TODO: Kategoriename anzeigen. "lernKategorieList[lernort.kategorieID].name" verursacht einen Fehler, von dem ich nicht weiß, wie ich ihn beheben soll. Funktioniert aber eigentlich. Deshalb auch auskommentiert. (Lisa) */
+                    Text("Kategorie",
+                        //lernKategorieList[lernort.kategorieID].name,
                         style: Theme.of(context).textTheme.headline4),
-                  ),
-                ]),
-                SizedBox(height: 20),
+                  ]),
+                  SizedBox(height: 20),
 
-                /* ÖFFNUNGSZEITEN */
-                Row(children: [
-                  Icon(FlutterIcons.clock_faw5s,
-                      size: 20, color: Color(0xff0b3e99)),
-                  SizedBox(width: 10),
+                  /* ADRESSE */
+                  Row(children: [
+                    Icon(FlutterIcons.location_arrow_faw5s,
+                        size: 20, color: Color(0xff0b3e99)),
+                    SizedBox(width: 10),
+                    Flexible(
+                      child: Text(
+                          lernort.adresse == ""
+                              ? "Keine Adresse vorhanden"
+                              : lernort.adresse,
+                          style: Theme.of(context).textTheme.headline4),
+                    )
+                  ]),
+                  SizedBox(height: 20),
+
+                  /* ÖFFNUNGSZEITEN */
+                  Row(children: [
+                    Icon(FlutterIcons.clock_faw5s,
+                        size: 20, color: Color(0xff0b3e99)),
+                    SizedBox(width: 10),
+                    Text(
+                        lernort.oeffnungszeiten == ""
+                            ? "Keine Öffnungszeiten vorhanden"
+                            : lernort.oeffnungszeiten,
+                        style: Theme.of(context).textTheme.headline4),
+                  ]),
+                  SizedBox(height: 40),
+
+                  /*BESCHREIBUNG*/
                   Text(
-                    lernort.oeffnungszeiten == ""
-                        ? "Keine Öffnungszeiten vorhanden"
-                        : lernort.oeffnungszeiten,
-                    style: Theme.of(context).textTheme.headline4
+                    lernort.kurzbeschreibung,
+                    textAlign: TextAlign.justify,
+                    style: Theme.of(context).textTheme.bodyText1,
                   ),
-                ]),
-                SizedBox(height: 40), */
-
-                /*BESCHREIBUNG*/
-                Text(
-                  lernort.kurzbeschreibung,
-                  textAlign: TextAlign.justify,
-                  style: Theme.of(context).textTheme.bodyText1,
-                ),
-                SizedBox(height: 50),
-                ImageButton(
-                  children: <Widget>[],
-                  /* 302 x 91 sind die Originalmaße der Buttons */
-                  width: 302 / 1.5,
-                  height: 91 / 1.5,
-                  paddingTop: 5,
-                  /* PressedImage gibt ein Bild für den Button im gedrückten 
+                  SizedBox(height: 50),
+                  ImageButton(
+                      children: <Widget>[],
+                      /* 302 x 91 sind die Originalmaße der Buttons */
+                      width: 302 / 1.5,
+                      height: 91 / 1.5,
+                      paddingTop: 5,
+                      /* PressedImage gibt ein Bild für den Button im gedrückten 
                     Zustand an. Bisher nicht implementiert, muss aber mit dem
                     Bild im normalen zustand angegeben werden. */
-                  pressedImage: Image.asset(
-                    "assets/buttons/Lernort_dunkelblau_groß.png",
-                  ),
-                  unpressedImage:
-                      Image.asset("assets/buttons/Lernort_dunkelblau_groß.png"),
-                  onTap: () {
-                    Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                            builder: (BuildContext context) =>
-                                LernortScreen(l: lernort, k: "TODO")));
-                  },
-                ),
-              ],
-            ),
-          )
-        ]))
-
-        /* CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              title: Text(lernort.name),
-              expandedHeight: 250,
-              flexibleSpace: FlexibleSpaceBar(
-                background: titelbild,
-              ),
-            ),
-            SliverList(
-                delegate: SliverChildListDelegate([
-              SizedBox(
-                height: 15,
-              ),
-              Row(
-                children: [
-                  // Expanded(
-                  //     flex: 1,
-                  //     child: Container(
-                  //       padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                  //       child: Text("Kategorie:",
-                  //           style: TextStyle(
-                  //               fontWeight: FontWeight.bold, fontSize: 20.0)),
-                  //     )),
-                  Expanded(
-                    flex: 1,
-                    child: Container(
-                        alignment: Alignment.centerRight,
-                        padding: EdgeInsets.fromLTRB(0, 0, 50, 0),
-                        child: kategorienSymbol),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Container(
-                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Beschreibung:",
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 20.0),
-                    ),
-                    SizedBox(height: 5),
-                    Text(lernort.kurzbeschreibung,
-                        style: Theme.of(context).textTheme.subtitle1,
-                        textAlign: TextAlign.justify),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Container(
-                padding: EdgeInsets.fromLTRB(50, 0, 50, 0),
-                child: ButtonTheme(
-                    child: RaisedButton(
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                            builder: (BuildContext context) =>
-                                LernortScreen(l: lernort, k: "TODO")));
-                  },
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18.0),
-                      side: BorderSide(color: Colors.blueGrey[900])),
-                  color: Colors.blueGrey[800],
-                  child: Text(
-                    "Weiter zum Lernort",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold, color: Colors.grey[100]),
-                  ),
-                )),
-              )
-            ]))
-          ],
-        ) */
-        ,
-      );
+                      pressedImage: Image.asset(
+                        "assets/buttons/Lernort_dunkelblau_groß.png",
+                      ),
+                      unpressedImage: Image.asset(
+                          "assets/buttons/Lernort_dunkelblau_groß.png"),
+                      onTap: () {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    LernortScreen(l: lernort, k: "TODO")));
+                      })
+                ]))
+          ])));
     }
   }
 }
