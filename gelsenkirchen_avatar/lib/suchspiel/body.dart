907adb989a238bce69c558da223b0c7e9478536d
simon.s <simon2204s@gmail.com>
2021-02-16 20:42:03 +0100

    Fehlenden import hinzugefügt

--- gelsenkirchen_avatar/lib/suchspiel/body.dart
+++ gelsenkirchen_avatar/lib/suchspiel/body.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:gelsenkirchen_avatar/suchspiel/suchspiel_art.dart';
