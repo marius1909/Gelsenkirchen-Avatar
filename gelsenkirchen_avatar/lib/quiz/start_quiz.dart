3394a6fe1b4be14b9e6b9ce401d438b0ddd672b4
phthao123 <69897117+phthao123@users.noreply.github.com>
2021-02-16 15:37:07 +0100

    ListView ergänzt

--- gelsenkirchen_avatar/lib/quiz/start_quiz.dart
+++ gelsenkirchen_avatar/lib/quiz/start_quiz.dart
@@ -20,17 +20,6 @@ class StartQuiz extends StatefulWidget {
 class _StartQuizState extends State<StartQuiz> {
   dynamic data;
 
-//  var quiz = new Quiz();
-//  @override
-//  void initState() {
-//    super.initState();
-//    var future = Quiz.shared.getQuiz(widget.l.id);
-//    future.then((data) {
-//      setState(() {
-//        quiz = data;
-//      });
-//    });
-//  }
   void getLernort() async {
     var id = widget.id;
     var url =
@@ -86,56 +75,19 @@ class _StartQuizState extends State<StartQuiz> {
             ),
 
             /* BESCHREIBUNG */
-            Container(
-              padding: EdgeInsets.fromLTRB(15, 0, 15, 40),
-              child: Text(
-                "Ein Spiel besteht aus 10 Fragen mit je 4 Antwortmöglichkeiten, von denen jeweils nur eine richtig ist. Für die Beantwortung einer Frage steht ein Zeitfenster von 30 Sekunden zu Verfügung. Zum Auswählen der gewünschten Antwort muss der Teilnehmer auf das jeweilige Antwortfeld klicken. Anschließend werden die Ergebnisse unten links auf dem Bildschirm angezeigt. Je mehr Fragen Sie beantworten, desto schwieriger werden sie. Je schwieriger die Frage ist, desto mehr Punkte erhalten Sie für die richtige Antwort. Wenn Sie eine falsche Antwort geben, werden Ihrem Konto keine Punkte hinzugefügt. Ziel des Spiels ist es, so viele Fragen wie möglich korrekt zu beantworten und die Belohnungen zu gelangen.",
-                textAlign: TextAlign.justify,
-                style: Theme.of(context).textTheme.bodyText1,
-              ),
-            ),
+            Expanded(
+                child: Container(
+                    child: Padding(
+                        padding: EdgeInsets.fromLTRB(15, 0, 15, 40),
+                        child: ListView(children: [
+                          Text(
+                            "Ein Spiel besteht aus 10 Fragen mit je 4 Antwortmöglichkeiten, von denen jeweils nur eine richtig ist. Für die Beantwortung einer Frage steht ein Zeitfenster von 30 Sekunden zu Verfügung. Zum Auswählen der gewünschten Antwort muss der Teilnehmer auf das jeweilige Antwortfeld klicken. Anschließend werden die Ergebnisse unten links auf dem Bildschirm angezeigt. Je mehr Fragen Sie beantworten, desto schwieriger werden sie. Je schwieriger die Frage ist, desto mehr Punkte erhalten Sie für die richtige Antwort. Wenn Sie eine falsche Antwort geben, werden Ihrem Konto keine Punkte hinzugefügt. Ziel des Spiels ist es, so viele Fragen wie möglich korrekt zu beantworten und die Belohnungen zu gelangen.",
+                            textAlign: TextAlign.justify,
+                            style: Theme.of(context).textTheme.bodyText1,
+                          ),
+                        ])))),
             SizedBox(height: 40),
-
-            //Belohnungen-Button (wird nicht mehr benötigt)
-/*             Padding(
-              padding: EdgeInsets.symmetric(vertical: 1),
-              child: Container(
-                  width: double.infinity,
-                  //Wrap with Material
-                  child: Material(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50.0)),
-                    clipBehavior: Clip.antiAlias,
-                    child: MaterialButton(
-                        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                        color: Colors.green[400],
-                        /*minWidth: 10.0,
-                          height: 35,*/
-                        onPressed: () {
-                          showDialog(
-                            context: context,
-                            barrierDismissible: false,
-                            builder: (BuildContext context) =>
-                                new CupertinoAlertDialog(
-                              title: new Text("Anzeigen der Belohnungen"),
-                              content: new Text("Loading....."),
-                              actions: [
-                                CupertinoDialogAction(
-                                  child: new Text("Yes"),
-                                  onPressed: () {
-                                    Navigator.of(context).pop(true);
-                                  },
-                                ),
-                              ],
-                            ),
-                          );
-                        },
-                        child: Text(
-                          "Belohnungen",
-                          style: TextStyle(fontSize: 18.0, color: Colors.white),
-                        )),
-                  )),
-            ), */
+            
 
             /* SPIELEN-BUTTON */
             ImageButton(
