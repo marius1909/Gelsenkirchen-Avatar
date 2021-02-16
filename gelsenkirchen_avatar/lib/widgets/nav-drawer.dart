ac209726c0209a08f1f8c39595b5aabebb098484
Alex <alexanderduenne@web.de>
2021-02-16 14:56:04 +0100

    Mergefehler behoben

--- gelsenkirchen_avatar/lib/widgets/nav-drawer.dart
+++ gelsenkirchen_avatar/lib/widgets/nav-drawer.dart
@@ -11,6 +11,7 @@ import 'package:gelsenkirchen_avatar/screens/anmeldung_screen.dart';
 import 'package:gelsenkirchen_avatar/screens/scoreboard_screen.dart';
 import 'package:gelsenkirchen_avatar/quiz/nfc_quiz.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:gelsenkirchen_avatar/suchspiel/suchspiel_screen.dart';
 
 class NavDrawer extends StatelessWidget {
   String status;
@@ -24,83 +25,78 @@ class NavDrawer extends StatelessWidget {
       status = "Abmelden";
       icon = "assets/icons/Abmelden_gelb_Icon.png";
     }
-    @override
-    Widget build(BuildContext context) {
-      return Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              child: Text(
-                '',
-                style: TextStyle(color: Colors.white, fontSize: 25),
-              ),
-              decoration: BoxDecoration(
-                  color: Colors.blue[200],
-                  image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: AssetImage('assets/images/Menubild.png'))),
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text(
+              '',
+              style: TextStyle(color: Colors.white, fontSize: 25),
             ),
+            decoration: BoxDecoration(
+                color: Colors.blue[200],
+                image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage('assets/images/Menubild.png'))),
+          ),
 
-            /* HOME (heißt in der App jetzt Karte, weil es den Menüpunkt für den Bentuzer treffender bezeichnet) */
-            ListTile(
-                //leading: Icon(Icons.home),
-                leading: ConstrainedBox(
-                  constraints: BoxConstraints(
-                    minWidth: 20,
-                    minHeight: 20,
-                    maxWidth: 30,
-                    maxHeight: 30,
-                  ),
-                  child: Image.asset("assets/icons/Home_dunkelblau_Icon.png"),
+          /* HOME (heißt in der App jetzt Karte, weil es den Menüpunkt für den Bentuzer treffender bezeichnet) */
+          ListTile(
+              //leading: Icon(Icons.home),
+              leading: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minWidth: 20,
+                  minHeight: 20,
+                  maxWidth: 30,
+                  maxHeight: 30,
                 ),
-                title: Text('Karte'),
-                onTap: () {
+                child: Image.asset("assets/icons/Home_dunkelblau_Icon.png"),
+              ),
+              title: Text('Karte'),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) => HomeScreen()));
+                //Error @Simon HomeScreen(angemeldeterBenutzer: global.user)));
+              }),
+
+          /* PROFIL */
+          ListTile(
+              //leading: Icon(Icons.face),
+              leading: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minWidth: 20,
+                  minHeight: 20,
+                  maxWidth: 30,
+                  maxHeight: 30,
+                ),
+                child: Image.asset("assets/icons/Profil_blau_Icon.png"),
+              ),
+              title: Text('Profil'),
+              onTap: () {
+                if (Benutzer.current?.id != null) {
                   Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (BuildContext context) => HomeScreen()));
-                  //Error @Simon HomeScreen(angemeldeterBenutzer: global.user)));
-                }),
+                          builder: (BuildContext context) =>
+                              Profil(Benutzer.current.id)));
+                } else {
+                  Fluttertoast.showToast(
+                      msg: "Anmeldung fehlt!", toastLength: Toast.LENGTH_SHORT);
+                }
+              }),
 
-            /* PROFIL */
-            ListTile(
-                //leading: Icon(Icons.face),
-                leading: ConstrainedBox(
-                  constraints: BoxConstraints(
-                    minWidth: 20,
-                    minHeight: 20,
-                    maxWidth: 30,
-                    maxHeight: 30,
-                  ),
-                  child: Image.asset("assets/icons/Profil_blau_Icon.png"),
-                ),
-                title: Text('Profil'),
-                onTap: () {
-                  if (Benutzer.current?.id != null) {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (BuildContext context) =>
-                                Profil(Benutzer.current.id)));
-                  } else {
-                    Fluttertoast.showToast(
-                        msg: "Anmeldung fehlt!",
-                        toastLength: Toast.LENGTH_SHORT);
-                  }
-                }),
-
-            /* LERNORTE */
-            ListTile(
-                //leading: Icon(Icons.account_balance),
-                leading: ConstrainedBox(
-                  constraints: BoxConstraints(
-                    minWidth: 20,
-                    minHeight: 20,
-                    maxWidth: 30,
-                    maxHeight: 30,
-                  ),
-                  child: Image.asset("assets/icons/Lernort_gelb_Icon.png"),
+          /* LERNORTE */
+          ListTile(
+              //leading: Icon(Icons.account_balance),
+              leading: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minWidth: 20,
+                  minHeight: 20,
+                  maxWidth: 30,
+                  maxHeight: 30,
                 ),
                 child: Image.asset("assets/icons/QR_rot_Icon.png"),
               ),
@@ -133,19 +129,19 @@ class NavDrawer extends StatelessWidget {
                         builder: (BuildContext context) => Freundesliste()));
               }), */
 
-            /* FREUNDE */
-            ListTile(
-                //leading: Icon(Icons.people),
-                leading: ConstrainedBox(
-                  constraints: BoxConstraints(
-                    minWidth: 20,
-                    minHeight: 20,
-                    maxWidth: 30,
-                    maxHeight: 30,
-                  ),
-                  child: Image.asset("assets/icons/Freunde_gruen_Icon.png"),
+          /* SCOREBOARD */
+          /* Wurde in Sprint 4 in "Bestelnliste" umbenannt, da verständlicher für User */
+          ListTile(
+              //leading: Icon(Icons.score),
+              leading: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minWidth: 20,
+                  minHeight: 20,
+                  maxWidth: 30,
+                  maxHeight: 30,
                 ),
-                child: Image.asset("assets/icons/Scoreboard_gruen_Icon.png"),
+                child:
+                    Image.asset("assets/icons/Scoreboard_dunkelblau_Icon.png"),
               ),
               title: Text('Bestenliste'),
               onTap: () {
@@ -153,42 +149,18 @@ class NavDrawer extends StatelessWidget {
                   Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (BuildContext context) => Freundesliste()));
-                }),
+                          builder: (BuildContext context) =>
+                              ScoreBoard(Benutzer.current.id)));
+                } else {
+                  Fluttertoast.showToast(
+                      msg: "Anmeldung fehlt!", toastLength: Toast.LENGTH_SHORT);
+                }
+              }),
 
-            /* SCOREBOARD */
-            /* Wurde in Sprint 4 in "Bestelnliste" umbenannt, da verständlicher für User */
-            ListTile(
-                //leading: Icon(Icons.score),
-                leading: ConstrainedBox(
-                  constraints: BoxConstraints(
-                    minWidth: 20,
-                    minHeight: 20,
-                    maxWidth: 30,
-                    maxHeight: 30,
-                  ),
-                  child: Image.asset(
-                      "assets/icons/Scoreboard_dunkelblau_Icon.png"),
-                ),
-                title: Text('Bestenliste'),
-                onTap: () {
-                  if (Benutzer.current?.id != null) {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (BuildContext context) =>
-                                ScoreBoard(Benutzer.current.id)));
-                  } else {
-                    Fluttertoast.showToast(
-                        msg: "Anmeldung fehlt!",
-                        toastLength: Toast.LENGTH_SHORT);
-                  }
-                }),
-
-            /* TODO: Hilfe muss noch implenentiert werden (optional) (Lisa)*/
-            /* TODO: Hilfeicon fehlt (optional) (Lisa) */
-            /* HILFE */
-            /* ListTile(
+          /* TODO: Hilfe muss noch implenentiert werden (optional) (Lisa)*/
+          /* TODO: Hilfeicon fehlt (optional) (Lisa) */
+          /* HILFE */
+          /* ListTile(
               leading: Icon(Icons.help),
               title: Text('Hilfe'),
               onTap: () {
@@ -198,10 +170,10 @@ class NavDrawer extends StatelessWidget {
                         builder: (BuildContext context) => HilfeScreen()));
               }), */
 
-            /* TODO: Einstellungen muss noch implenentiert werden (optional) (Lisa) */
-            /* TODO: Einstellungenicon fehlt (optional) (Lisa) */
-            /* EINSTELLUNGEN */
-            /* ListTile(
+          /* TODO: Einstellungen muss noch implenentiert werden (optional) (Lisa) */
+          /* TODO: Einstellungenicon fehlt (optional) (Lisa) */
+          /* EINSTELLUNGEN */
+          /* ListTile(
               leading: Icon(Icons.settings),
               title: Text('Einstellungen'),
               onTap: () {
@@ -212,17 +184,15 @@ class NavDrawer extends StatelessWidget {
                             EinstellungenScreen()));
               }), */
 
-            /* IMPRESSUM */
-            ListTile(
-                //leading: Icon(Icons.description),
-                leading: ConstrainedBox(
-                  constraints: BoxConstraints(
-                    minWidth: 20,
-                    minHeight: 20,
-                    maxWidth: 30,
-                    maxHeight: 30,
-                  ),
-                  child: Image.asset("assets/icons/Impressum_blau_Icon.png"),
+          /* IMPRESSUM */
+          ListTile(
+              //leading: Icon(Icons.description),
+              leading: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minWidth: 20,
+                  minHeight: 20,
+                  maxWidth: 30,
+                  maxHeight: 30,
                 ),
                 child:
                     Image.asset("assets/icons/Impressum_dunkelblau_Icon.png"),
@@ -235,34 +205,33 @@ class NavDrawer extends StatelessWidget {
                         builder: (BuildContext context) => ImpressumScreen()));
               }),
 
-            /* ANMELDEN / ABMELDEN je nachdem, ob Benutzer angemeldet*/
-            ListTile(
-                //leading: Icon(Icons.description),
-                leading: ConstrainedBox(
-                  constraints: BoxConstraints(
-                    minWidth: 20,
-                    minHeight: 20,
-                    maxWidth: 30,
-                    maxHeight: 30,
-                  ),
-                  child: Image.asset(icon),
+          /* ANMELDEN / ABMELDEN je nachdem, ob Benutzer angemeldet*/
+          ListTile(
+              //leading: Icon(Icons.description),
+              leading: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minWidth: 20,
+                  minHeight: 20,
+                  maxWidth: 30,
+                  maxHeight: 30,
                 ),
-                title: Text(status),
-                onTap: () async {
-                  if (Benutzer.current?.id != null) {
-                    Benutzer.current = null;
-                    SharedPreferences sharedPreferences =
-                        await SharedPreferences.getInstance();
-                    sharedPreferences.remove("benutzer");
-                  }
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (BuildContext context) => Anmeldung()));
-                }),
-          ],
-        ),
-      );
-    }
+                child: Image.asset(icon),
+              ),
+              title: Text(status),
+              onTap: () async {
+                if (Benutzer.current?.id != null) {
+                  Benutzer.current = null;
+                  SharedPreferences sharedPreferences =
+                      await SharedPreferences.getInstance();
+                  sharedPreferences.remove("benutzer");
+                }
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) => Anmeldung()));
+              }),
+        ],
+      ),
+    );
   }
 }
