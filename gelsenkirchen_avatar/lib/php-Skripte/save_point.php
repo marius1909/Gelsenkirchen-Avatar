b18dd8359b7d01e12c838193f9144a40f887f473
phthao123 <69897117+phthao123@users.noreply.github.com>
2021-04-10 17:51:37 +0200

    Punktzahlspeicherung für Memory_,Suchspiel ergänzen

--- gelsenkirchen_avatar/lib/php-Skripte/save_point.php
+++ gelsenkirchen_avatar/lib/php-Skripte/save_point.php
@@ -5,6 +5,8 @@ $lernKategorieID = $_GET['lernKategorieID'];
 $erfahrungspunkte = $_GET['erfahrungspunkte'];
 $lernortID = $_GET['lernortID'];
 $quizID = $_GET['quizID'];
+$memoryID = $_GET['memoryID'];
+$suchID = $_GET['suchID'];
 /*Punkte speichern*/
 $query = "select SUM(erfahrungspunkte) as erfahrungspunkte from BenutzerKategorie where benutzerID = $benutzerID";
 $res = mysqli_query($con,$query);
@@ -13,11 +15,25 @@ while ($row = mysqli_fetch_assoc($res)) {
     $pointOld = $row['erfahrungspunkte'] != null ? intval($row['erfahrungspunkte']) : 0;
 }
 
-$query = "delete from BenutzerKategorie where lernKategorieID = $lernKategorieID and benutzerID = $benutzerID and lernortID = $lernortID and quizID = $quizID ";
-mysqli_query($con,$query);
-$query ="insert into BenutzerKategorie (benutzerID,lernKategorieID,erfahrungspunkte,lernortID,quizID,created_at) VALUES ($benutzerID,$lernKategorieID,$erfahrungspunkte,$lernortID,$quizID,'".date("Y-m-d H:i:s")."');";
-mysqli_query($con, $query);
+if($quizID != null){
+    $query = "delete from BenutzerKategorie where lernKategorieID = $lernKategorieID and benutzerID = $benutzerID and lernortID = $lernortID and quizID = $quizID";
+} elseif($memoryID != null) {
+    $query = "delete from BenutzerKategorie where lernKategorieID = $lernKategorieID and benutzerID = $benutzerID and lernortID = $lernortID and memoryID = $memoryID";
+} else {
+    $query = "delete from BenutzerKategorie where lernKategorieID = $lernKategorieID and benutzerID = $benutzerID and lernortID = $lernortID and suchID = $suchID";
+}
 
+mysqli_query($con,$query);
+
+if($quizID != null){
+    $query ="insert into BenutzerKategorie (benutzerID,lernKategorieID,erfahrungspunkte,lernortID,quizID,created_at) VALUES ($benutzerID,$lernKategorieID,$erfahrungspunkte,$lernortID,$quizID,'".date("Y-m-d H:i:s")."');";
+} elseif($memoryID != null) {
+    $query ="insert into BenutzerKategorie (benutzerID,lernKategorieID,erfahrungspunkte,lernortID,memoryID,created_at) VALUES ($benutzerID,$lernKategorieID,$erfahrungspunkte,$lernortID,$memoryID,'".date("Y-m-d H:i:s")."');";
+} else {
+    $query ="insert into BenutzerKategorie (benutzerID,lernKategorieID,erfahrungspunkte,lernortID,suchID,created_at) VALUES ($benutzerID,$lernKategorieID,$erfahrungspunkte,$lernortID,$suchID,'".date("Y-m-d H:i:s")."');";
+}
+
+mysqli_query($con, $query);
 /*Punkte für jede Kategorie berechnen (für Rangliste nach Kategorie)*/
 $query = "select SUM(erfahrungspunkte) as erfahrungspunkte from BenutzerKategorie where benutzerID = $benutzerID AND lernKategorieID = $lernKategorieID;";
 $res = mysqli_query($con,$query);
@@ -25,16 +41,18 @@ $point = 0;
 while ($row = mysqli_fetch_assoc($res)) {
     $point = $row['erfahrungspunkte'] != null ? intval($row['erfahrungspunkte']) : 0;
 }
-
 $query = "select SUM(erfahrungspunkte) as erfahrungspunkte from BenutzerKategorie where benutzerID = $benutzerID";
 $res = mysqli_query($con,$query);
+
 $pointNew = 0;
 while ($row = mysqli_fetch_assoc($res)) {
     $pointNew = $row['erfahrungspunkte'] != null ? intval($row['erfahrungspunkte']) : 0;
 }
 mysqli_free_result($res);
+/*löschen Daten von RankKategorie von dem Benutzer, wenn die schon existiert haben*/
 $query = "delete from RankKategorie where lernKategorieID = $lernKategorieID and benutzerID = $benutzerID";
 mysqli_query($con,$query);
+/*neu Punktzahl aktualiesiert*/
 $query ="insert into RankKategorie (benutzerID,lernKategorieID,sum_erfahrungspunkte,created_at) VALUES ($benutzerID,$lernKategorieID,$point,'".date("Y-m-d H:i:s")."');";
 mysqli_query($con, $query);
 
@@ -45,3 +63,4 @@ $data['total_point_old'] = $pointOld;
 $data['status'] = true;
 echo json_encode($data);
 ?>
+
