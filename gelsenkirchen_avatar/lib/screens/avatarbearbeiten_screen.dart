a8da44b897d88824e46391ae3436f8b1c33a4235
Marius <marius.weise@web.de>
2021-04-19 00:56:47 +0200

    Besserer ayncs loader für Avatarbearbeiten_Screen

--- gelsenkirchen_avatar/lib/screens/avatarbearbeiten_screen.dart
+++ gelsenkirchen_avatar/lib/screens/avatarbearbeiten_screen.dart
@@ -13,12 +13,18 @@ class Avatarbearbeiten extends StatefulWidget {
 }
 
 class _AvatarbearbeitenState extends State<Avatarbearbeiten> {
+  var _result;
   List<String> auswaehlbareAvatare = new List();
 
   @override
   void initState() {
     super.initState();
-    asyncInitState();
+
+    loadAsyncData().then((result) {
+      setState(() {
+        _result = result;
+      });
+    });
   }
 
   @override
@@ -27,75 +33,79 @@ class _AvatarbearbeitenState extends State<Avatarbearbeiten> {
     auswaehlbareAvatare.add(Avatar.getDefaultImagePath(1));
     auswaehlbareAvatare.add(Avatar.getDefaultImagePath(2));
     auswaehlbareAvatare.add(Avatar.getDefaultImagePath(3));
-
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Avatar auswählen'),
-        ),
-        body: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
-              child: Text("Wähle deinen Avatar",
-                  style: Theme.of(context).textTheme.headline3),
-            ),
-            Container(
-              padding: EdgeInsets.fromLTRB(0, 0, 0, 50),
-              child: CarouselSlider.builder(
-                  itemCount: auswaehlbareAvatare.length,
-                  itemBuilder: (context, index) {
-                    print(auswaehlbareAvatare.length);
-                    return Container(
-                      margin: EdgeInsets.all(6.0),
-                      child:
-                          Image.asset(auswaehlbareAvatare[index], height: 300),
-                    );
-                  },
-                  options: CarouselOptions(
-                    height: 100,
-                    enlargeCenterPage: true,
-                    autoPlay: false,
-                    aspectRatio: 16 / 9,
-                    autoPlayCurve: Curves.fastOutSlowIn,
-                    enableInfiniteScroll: true,
-                    autoPlayAnimationDuration: Duration(milliseconds: 800),
-                    viewportFraction: 0.3,
-                  )),
-            ),
-            ImageButton(
-              children: <Widget>[],
-              /* 302 x 91 sind die Originalmaße der Buttons */
-              width: 302 / 1.3,
-              height: 91 / 1.3,
-              paddingTop: 0,
-              /* PressedImage gibt ein Bild für den Button im gedrückten 
-                    Zustand an. Bisher nicht implementiert, muss aber mit dem
-                    Bild im normalen zustand angegeben werden. */
-              pressedImage: Image.asset(
-                "assets/buttons/Speichern_dunkelblau_groß.png",
+    if (_result == null) {
+      return new Container();
+    } else {
+      return Scaffold(
+          appBar: AppBar(
+            title: Text('Avatar auswählen'),
+          ),
+          body: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
+                child: Text("Wähle deinen Avatar",
+                    style: Theme.of(context).textTheme.headline3),
               ),
-              unpressedImage:
-                  Image.asset("assets/buttons/Speichern_dunkelblau_groß.png"),
-              onTap: () {
-                /* TODO: (nicht bis S&T machbar) Ausgewählten Avatar in DB Speichern (Lisa) */
+              Container(
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 50),
+                child: CarouselSlider.builder(
+                    itemCount: auswaehlbareAvatare.length,
+                    itemBuilder: (context, index) {
+                      print(auswaehlbareAvatare.length);
+                      return Container(
+                        margin: EdgeInsets.all(6.0),
+                        child: Image.asset(auswaehlbareAvatare[index],
+                            height: 300),
+                      );
+                    },
+                    options: CarouselOptions(
+                      height: 100,
+                      enlargeCenterPage: true,
+                      autoPlay: false,
+                      aspectRatio: 16 / 9,
+                      autoPlayCurve: Curves.fastOutSlowIn,
+                      enableInfiniteScroll: true,
+                      autoPlayAnimationDuration: Duration(milliseconds: 800),
+                      viewportFraction: 0.3,
+                    )),
+              ),
+              ImageButton(
+                children: <Widget>[],
+                /* 302 x 91 sind die Originalmaße der Buttons */
+                width: 302 / 1.3,
+                height: 91 / 1.3,
+                paddingTop: 0,
+                /* PressedImage gibt ein Bild für den Button im gedrückten 
+                        Zustand an. Bisher nicht implementiert, muss aber mit dem
+                        Bild im normalen zustand angegeben werden. */
+                pressedImage: Image.asset(
+                  "assets/buttons/Speichern_dunkelblau_groß.png",
+                ),
+                unpressedImage:
+                    Image.asset("assets/buttons/Speichern_dunkelblau_groß.png"),
+                onTap: () {
+                  /* TODO: (nicht bis S&T machbar) Ausgewählten Avatar in DB Speichern (Lisa) */
 
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => Profil(Benutzer.current.id)),
-                );
-              },
-            ),
-          ],
-        ));
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => Profil(Benutzer.current.id)),
+                  );
+                },
+              ),
+            ],
+          ));
+    }
   }
 
-  void asyncInitState() async {
+  Future<bool> loadAsyncData() async {
     List<String> a =
         await Avatar.getAuswaehlbareAvatareList(Benutzer.current.id);
     print("he");
     setState(() {
       auswaehlbareAvatare = a;
     });
+    return true;
   }
 }
